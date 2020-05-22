1d90951e443ee6702c7028c1f19d0f434e49a28e
duanhaobin <1807227469@qq,com>
2020-05-22 09:44:28 +0800

    '补充上次提交内容'

--- codes/Part_1_Web_Service/Chapter_1_Go_and_Web_applications/ch1.2_路由注册/ch1.2.2_路由管理/domain_registration.go
+++ codes/Part_1_Web_Service/Chapter_1_Go_and_Web_applications/ch1.2_路由注册/ch1.2.2_路由管理/domain_registration.go
@@ -3,3 +3,19 @@
 //Date  : 2020/5/22
 
 package main
+
+import (
+	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/net/ghttp"
+)
+
+func main() {
+	/*
+		域名注册实例
+	*/
+	d := g.Server().Domain("localhost")
+	d.BindHandler("/index", func(r *ghttp.Request) {
+		r.Response.Writef("域名注册:%v", r.Router)
+	})
+	g.Server().Run()
+}
